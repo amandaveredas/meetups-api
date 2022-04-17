26225bdd11cca7a878b429c0ce328d3e8ca055b8
amandaveredas <amandaveredas@gmail.com>
2022-04-17 02:44:48 -0300

    FIX: Resolves problems with swagger-ui Inserts ressource handlers in the SwaggerConfig.class and annotations

--- src/main/java/com/microservicemeetup/config/SwaggerConfig.java
+++ src/main/java/com/microservicemeetup/config/SwaggerConfig.java
@@ -2,25 +2,29 @@ package com.microservicemeetup.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-
+@EnableWebMvc
+@Import(SpringDataRestConfiguration.class)
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis( RequestHandlerSelectors.basePackage("com.bootcamp.microservicemeetup.controller.resource") )
+                .apis( RequestHandlerSelectors.basePackage("com.microservicemeetup.controller.resource"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
@@ -39,5 +43,14 @@ public class SwaggerConfig {
         return new Contact("Amanda Lima Santos",
                 "https://github.com/amandaveredas",
                 "amandaveredas@gmail.com");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
